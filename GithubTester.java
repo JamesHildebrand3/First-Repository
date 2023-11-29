8a418a1b67b70c69667c62295d8fb04e3f050105
JamesHildebrand3 <152349956+JamesHildebrand3@users.noreply.github.com>
2023-11-29 12:32:36 -0500

    Added GithubTester class
    This class prints the phrase "Hello world" and loops it 10 times.

---
+++ GithubTester.java
@@ -0,0 +1,12 @@
+public class GithubTester
+{
+   public static void main(String [] args)
+   {
+      int x;
+      for(x = 0; x < 10; x++)
+      {
+         System.out.println("Hello world");
+      }
+   }
+}
+  
